c1d9083d745a9a27c24d599f7f5f375db8643946
adriano.souza <adriano.souza@zpesystems.com>
2022-06-27 16:42:37 -0300

    Fixing typo

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -75,7 +75,7 @@ export function Video(props: VideoProps) {
 
             <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
               <Lightning size={24} />
-              Accesse o desafio
+              Acesse o desafio
             </a>
           </div>
         </div>
